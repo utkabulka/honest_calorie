db2dbfccf8bfe1ce0a7886441cbe5895bcd90e42
Roman Zubin <utkabulka@gmail.com>
2025-03-17 18:22:06 +0400

    persist app settings

--- lib/src/common/theme.dart
+++ lib/src/common/theme.dart
@@ -15,3 +15,32 @@ const defaultRoundedRectangleTop = RoundedRectangleBorder(
 // Shadows
 const subtleShadow =
     BoxShadow(color: Colors.black12, offset: Offset(0, 0), blurRadius: 5);
+
+// Prefs
+const String PREF_THEME_MODE = "themeMode";
+const String PERF_THEME_SYSTEM = "system";
+const String PERF_THEME_LIGHT = "light";
+const String PERF_THEME_DARK = "dark";
+
+ThemeMode getThemeModeFromKey(String key) {
+  switch (key) {
+    case PERF_THEME_LIGHT:
+      return ThemeMode.light;
+    case PERF_THEME_DARK:
+      return ThemeMode.dark;
+    case PERF_THEME_SYSTEM:
+    default:
+      return ThemeMode.system;
+  }
+}
+
+String themeModeToPrefKey(ThemeMode themeMode) {
+  switch (themeMode) {
+    case ThemeMode.light:
+      return PERF_THEME_LIGHT;
+    case ThemeMode.dark:
+      return PERF_THEME_DARK;
+    case ThemeMode.system:
+      return PERF_THEME_SYSTEM;
+  }
+}
--- lib/src/models/app_settings_model.dart
+++ lib/src/models/app_settings_model.dart
@@ -4,12 +4,30 @@ Copyright (C) 2025 Nicole Zubina
 Full notice can be found at /lib/main.dart file. */
 
 import 'package:flutter/material.dart';
+import 'package:honest_calorie/src/common/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSettingsModel extends ChangeNotifier {
+  late SharedPreferencesAsync prefs;
+
+  AppSettingsModel() {
+    _initPrefs();
+  }
+
+  Future _initPrefs() async {
+    prefs = SharedPreferencesAsync();
+
+    _themeMode = getThemeModeFromKey(
+        await prefs.getString(PREF_THEME_MODE) ?? PERF_THEME_SYSTEM);
+
+    notifyListeners();
+  }
+
   ThemeMode _themeMode = ThemeMode.system;
   ThemeMode get themeMode => _themeMode;
   set themeMode(ThemeMode value) {
     _themeMode = value;
+    prefs.setString(PREF_THEME_MODE, themeModeToPrefKey(_themeMode));
     notifyListeners();
   }
 
